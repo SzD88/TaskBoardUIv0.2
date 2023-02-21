4ec4d771a3f7ed3bff8b99a4f65305598a9e5e50
S.D <szopakleks@gmail.com>
2023-02-21 13:58:24 +0100

    Task

--- src/app/day-detail/day-detail.component.ts
+++ src/app/day-detail/day-detail.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { Day } from '../entities/Day';
 import { DaysComponent } from '../days/days.component';
 import { DaysService } from '../services/days.service';
- 
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+
+
 @Component({
   selector: 'app-day-detail',
   templateUrl: './day-detail.component.html',
   styleUrls: ['./day-detail.component.css']
 }) 
-export class DayDetailComponent
+export class DayDetailComponent implements OnInit
 {
+  inputs = [1];
 
   changedDay?: Day;
 
@@ -22,10 +25,21 @@ export class DayDetailComponent
     private route: ActivatedRoute,
     private projects: DaysComponent,
 
-  ) { }
+  ) {
+    
+  }
 
   ngOnInit() {
-   
+
+     
+
+  }
+
+  addInput() {
+    console.log("test add input");
+
+    this.ngOnInit();
+    // this.inputs.push(1);
   }
 
   overrideDay(id: string, projectNumber: string, title: string, description: string, completed: boolean) {
@@ -34,21 +48,28 @@ export class DayDetailComponent
   onChangesSubmited(project: Day): void {
     var jsn = JSON.stringify(this.day);
     this.daysService.updateDay(JSON.parse(jsn));
-
   }
-
+  
   addNewAsCurrent(project: Day): void {
     var jsn = JSON.stringify(this.day);
     this.daysService.addDay(JSON.parse(jsn));
   }
-  getCurrentDay(id: number) {
+  getCurrentDay( ) {
 
-    console.log(this.day?.dayDate); // masz te id
-    console.log(this.day); // masz te id 
+    console.log("from method");
+    console.log(this.day);
+
     
   }
   deleteCurrentDay(id: number) {  
     this.daysService.testDelete(id).subscribe(() => { 
     }) 
-  }  
+  }
+ 
+
+   
+
+ 
+
+ 
 }
