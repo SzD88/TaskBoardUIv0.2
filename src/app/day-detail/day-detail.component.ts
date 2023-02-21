6ab4514524d63ed6ab2ddec9a5763fb6c9de4b7f
John Szop <szopakleks@hotmail.com>
2023-02-21 05:53:39 +0100

    Even

--- src/app/day-detail/day-detail.component.ts
+++ src/app/day-detail/day-detail.component.ts
@@ -4,17 +4,12 @@ import { Observable, Subscription } from 'rxjs';
 import { Day } from '../entities/Day';
 import { DaysComponent } from '../days/days.component';
 import { DaysService } from '../services/days.service';
-
-
-
+ 
 @Component({
   selector: 'app-day-detail',
   templateUrl: './day-detail.component.html',
   styleUrls: ['./day-detail.component.css']
-})
-
-
-
+}) 
 export class DayDetailComponent
 {
 
@@ -47,8 +42,10 @@ export class DayDetailComponent
     this.daysService.addDay(JSON.parse(jsn));
   }
   getCurrentDay(id: number) {
-   return this.daysService.getDayById(this.day!.id).subscribe(() => {
-    })
+
+    console.log(this.day?.dayDate); // masz te id
+    console.log(this.day); // masz te id 
+    
   }
   deleteCurrentDay(id: number) {  
     this.daysService.testDelete(id).subscribe(() => { 
--- src/app/entities/Day.ts
+++ src/app/entities/Day.ts
@@ -2,7 +2,7 @@ import { Task } from "./Task";
 
 export interface Day {
   id: number;
-  projectNumber: string;
+  dayDate: string;
   title: string;
   description: string;
   completed: boolean;
